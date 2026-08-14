84ab1a54e45949a4f7d477ee0ff827ff562f9f17
dddddqz <daiqizhi2@gmail.com>
2026-08-14 06:06:15 -0400

    补充

--- level_1.gd
+++ level_1.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var player: CharacterBody2D = $CharacterBody2D
 @onready var door: Area2D = $Area2D 
 
-var is_player_in_door: bool = false
+#var is_player_in_door: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +11,12 @@ func _ready() -> void:
 	door.played_out_door.connect(_on_played_out_door)
 
 func _on_played_in_door():
-	is_player_in_door = true
+	#is_player_in_door = true
+
 	print("player in door")
 func _on_played_out_door():
-	is_player_in_door = false
+	#is_player_in_door = false
+	
 	print("player out door")
 
 
